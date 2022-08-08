42209b83901ca114f18c4fd728699fba1ddda109
Omar Mirza <omarasimmirza@gmail.com>
2022-08-08 16:09:11 +0300

    commit v1.3

--- db/info_database.py
+++ db/info_database.py
@@ -3,4 +3,9 @@ import sqlalchemy as sql
 base = sql.declarative_base()
 class Stats(base):
     __tablename__ = "machines"
-    ip = sql.Column(sql.String, primary_key=True)
+    ip = sql.Column('ip', sql.String, primary_key=True)
+    port = sql.Column('port', sql.Integer)
+    username = sql.Column('username', sql.String)
+    mail = sql.Column('mail', sql.String)
+    alert_type = sql.Column('alert_type', sql.String)
+    alert_limit = sql.Column('alert_limit', sql.String)
--- server/info_server.py
+++ server/info_server.py
@@ -6,6 +6,9 @@ import os
 checker = libserver.SystemCheck()
 checker.parse_xml("data.xml")
 checker.ssh_connect()
-load_dotenv()
-store = db_connection.MySQL_connect(os.environ.get("user"), os.environ.get("password"), os.environ.get("server"))
-store.create_connection()
+# load_dotenv()
+# store = db_connection.MySQL_connect(os.environ.get("user"), os.environ.get("password"), os.environ.get("server"))
+# store.create_connection()
+
+# class machines(store.db.Model):
+#     ip = store.db.Column(store.db.Integer)
